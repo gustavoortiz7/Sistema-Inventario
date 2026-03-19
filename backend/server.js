792db85a329bae4f6b202dac922d318114eb6ac2
Gustavoortiz7 <lortizgustavol@gmail.com>
2026-03-19 18:12:30 -0400

    add JWT autenticacion

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./src/config/db');
+const authRoutes = require('./src/routes/authRoutes');
+
+dotenv.config();
+
+connectDB();
+
 const app = express();
 
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.send('Api Inventario Funcionando');
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
