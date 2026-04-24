dc35211ab9505cd8c1ad3ab4872cb3b766004cfa
Gustavoortiz7 <lortizgustavol@gmail.com>
2026-04-24 01:31:35 -0400

    Add  tailwind styling to frontend

--- backend/server.js
+++ backend/server.js
@@ -13,28 +13,15 @@ const app = express();
 
 const cors = require('cors');
 
-// 🔥 CONFIGURACIÓN SEGURA
-const corsOptions = {
+app.use(cors({
     origin: 'https://sistema-inventario-frontend-black.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-};
+    credentials: true
+}));
 
-// usar CORS
-app.use(cors(corsOptions));
-
-// 🔥 RESPONDER MANUALMENTE AL PREFLIGHT
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://sistema-inventario-frontend-black.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
-
-    next();
-});
+// 👇 IMPORTANTE para preflight
+app.options('*', cors());
 
 
 app.use(express.json());
